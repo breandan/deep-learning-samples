a26003d3d2223c432a9d77a7e34fd84e7701d3a9
breandan.considine <breandan.considine@gmail.com>
2016-06-28 15:37:31 -0300

    Initial commit

---
+++ src/main/java/MnistPretrained.java
@@ -0,0 +1,56 @@
+import org.canova.api.records.reader.RecordReader;
+import org.canova.api.split.FileSplit;
+import org.canova.image.recordreader.ImageRecordReader;
+import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
+import org.deeplearning4j.datasets.iterator.DataSetIterator;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.io.*;
+import java.util.*;
+
+public class MnistPretrained {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        MultiLayerNetwork lenet = ModelSerializer.restoreMultiLayerNetwork(new File(classLoader.getResource("lecun_mnist").getFile()));
+        MultiLayerNetwork mlp1 = ModelSerializer.restoreMultiLayerNetwork(new File(classLoader.getResource("mlp1_mnist").getFile()));
+        MultiLayerNetwork mlp2 = ModelSerializer.restoreMultiLayerNetwork(new File(classLoader.getResource("mlp2_mnist").getFile()));
+        MultiLayerNetwork sae = ModelSerializer.restoreMultiLayerNetwork(new File(classLoader.getResource("sae_mnist").getFile()));
+
+        List<String> labels = new ArrayList<>(10);
+        for(int i = 0; i < 10; i++) {
+            labels.add(i + "");
+        }
+
+        RecordReader recordReader = new ImageRecordReader(28, 28, 1);
+        recordReader.initialize(new FileSplit(new File(classLoader.getResource("./").getFile())));
+
+        DataSetIterator dataSet = new RecordReaderDataSetIterator(recordReader, 1);
+
+        while(dataSet.hasNext()) {
+            INDArray indArray = dataSet.next().getFeatureMatrix();
+            float[] f = indArray.data().asFloat();
+            float[] t = new float[f.length];
+            for(int i = 0; i < f.length; i++) {
+                if(f[i] < 255)
+                    t[i] = 1;
+                else
+                    t[i] = 0;
+            }
+            indArray = Nd4j.create(t);
+
+            INDArray lenetOutput = lenet.output(indArray);
+            INDArray mlp1Output = mlp1.output(indArray);
+            INDArray mlp2Output = mlp2.output(indArray);
+
+            System.out.println(lenetOutput + " <- LeNet (Convolutional Neural Net)");
+            System.out.println(mlp1Output + " <- 1-Layer Multilayer Perceptron");
+            System.out.println(mlp2Output + " <- 2-Layer Multilayer Perceptron");
+
+            System.out.println("   ^     ^     ^     ^     ^     ^     ^     ^     ^     ^");
+            System.out.println("   0     1     2     3     4     5     6     7     8     9");
+        }
+    }
+}
---
+++ src/main/java/Perceptron.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class Perceptron {
+    public static void main(String[] args) {
+        double[][] values = new double[][]{{0, 0, 0},{1, 1, 1}, {2, 1, 0}};
+        double[] weights = new double[]{0, 0, 0};
+
+        //Training
+        for(int e = 0; e < 100; e++) {
+            double totalError = 0;
+
+            for (double[] value : values) {
+                double actual = value[2];
+                double error = actual - classify(value, weights);
+                totalError += Math.abs(error);
+                weights[0] += error * 1;
+                for (int i = 1; i < weights.length; i++) {
+                    weights[i] += error * value[i-1];
+                }
+            }
+            System.out.println("Total error: " + totalError);
+            System.out.println(Arrays.toString(weights));
+            if(totalError == 0)
+                break;
+        }
+
+        System.out.println(classify(new double[]{0, 0}, weights));
+        System.out.println(classify(new double[]{0, 1}, weights));
+        System.out.println(classify(new double[]{2, 1}, weights));
+        System.out.println(classify(new double[]{1, 1}, weights));
+
+        System.out.println(Arrays.toString(weights));
+    }
+
+    private static double classify(double[] value, double[] weights) {
+        double pred = 1 * weights[0];
+        for(int i = 1; i < weights.length; i++) {
+            pred += weights[i] * value[i - 1];
+        }
+
+        if(pred < 0) {
+            return 0;
+        }
+
+        return 1;
+    }
+}
