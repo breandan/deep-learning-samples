17ba435d1743b480f1f8c35bfae0ae6d94cd29b8
breandan <breandan.considine@gmail.com>
2017-02-05 13:51:16 +0100

    update to reflect parent structure

--- jfokus-examples/src/main/java/MnistPretrained.java
+++ jfokus-examples/src/main/java/MnistPretrained.java
@@ -1,11 +1,11 @@
-import org.canova.api.records.reader.RecordReader;
-import org.canova.api.split.FileSplit;
-import org.canova.image.recordreader.ImageRecordReader;
-import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
-import org.deeplearning4j.datasets.iterator.DataSetIterator;
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.split.FileSplit;
+import org.datavec.image.recordreader.ImageRecordReader;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.factory.Nd4j;
 
 import java.io.*;
